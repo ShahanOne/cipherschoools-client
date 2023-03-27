412f0a7d877ce71e7c0c73e15eebb9850af0067e
Shahan.One <shahanhussain9105@gmail.com>
2023-03-27 23:51:02 +0530

    form changes

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -5,7 +5,22 @@ import { useState } from 'react';
 
 function UserPage(props) {
   const [isMyFollowersClicked, setMyFollowersClick] = useState(false);
-  const [newComment, setComment] = useState('');
+  const [webInput, setWebInput] = useState(true);
+  const [webButton, setWeb] = useState('Edit');
+  const [infoInput, setInfoInput] = useState(true);
+  const [infoButton, setInfo] = useState('Edit');
+  const [passwordInput, setPasswordInput] = useState(true);
+  const [passwordButton, setPass] = useState('Change');
+
+  const [linkedin, setLinkedin] = useState('');
+  const [github, setGithub] = useState('');
+  const [facebook, setFacebook] = useState('');
+  const [instagram, setInstagram] = useState('');
+  const [twitter, setTwitter] = useState('');
+  const [website, setWebsite] = useState('');
+  const [education, setEducation] = useState('');
+  const [college, setCollege] = useState('');
+  const [password, setPassword] = useState('');
 
   function handleHome() {
     setMyFollowersClick(false);
@@ -18,22 +33,146 @@ function UserPage(props) {
     // window.location.reload();
     props.handleSignOut();
   }
+  function handleLinkedinChange(e) {
+    const { value } = e.target;
 
+    setLinkedin(value);
+  }
+  function handleGithubChange(e) {
+    const { value } = e.target;
+
+    setGithub(value);
+  }
+  function handleFacebookChange(e) {
+    const { value } = e.target;
+
+    setFacebook(value);
+  }
+  function handleInstaChange(e) {
+    const { value } = e.target;
+
+    setInstagram(value);
+  }
+  function handleTwitterChange(e) {
+    const { value } = e.target;
+
+    setTwitter(value);
+  }
+  function handleWebsiteChange(e) {
+    const { value } = e.target;
+
+    setWebsite(value);
+  }
+  function handleEducationChange(e) {
+    const { value } = e.target;
+
+    setEducation(value);
+  }
+  function handleCollegeChange(e) {
+    const { value } = e.target;
+
+    setCollege(value);
+  }
+  function handlePasswordChange(e) {
+    const { value } = e.target;
+
+    setPassword(value);
+  }
+  function handleWebEdit() {
+    setWebInput(false);
+    setWeb('Save');
+  }
+
+  function handleInfoEdit() {
+    setInfoInput(false);
+    setInfo('Save');
+  }
+  function handlePasswordEdit() {
+    setPasswordInput(false);
+    setPass('Save');
+  }
+
+  async function handleWebInfo() {
+    try {
+      const res = await fetch('http://localhost:3001/web-info', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([
+          {
+            userId: props.userId,
+            linkedin: linkedin,
+            github: github,
+            facebook: facebook,
+            instagram: instagram,
+            twitter: twitter,
+            website: website,
+          },
+        ]),
+      })
+        .then((res) => res.json())
+        .then((data) => data !== 'poop' && props.newUserData(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async function handlePersonalInfo() {
+    try {
+      const res = await fetch('http://localhost:3001/personal-info', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([
+          {
+            userId: props.userId,
+            education: education,
+            college: college,
+          },
+        ]),
+      })
+        .then((res) => res.json())
+        .then((data) => data !== 'poop' && props.newUserData(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async function handlePasswordInfo() {
+    try {
+      const res = await fetch('http://localhost:3001/new-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([
+          {
+            userId: props.userId,
+            password: password,
+          },
+        ]),
+      })
+        .then((res) => res.json())
+        .then((data) => data !== 'poop' && props.newUserData(data));
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <>
-      <div className="font-fira py-4 px-4 md:px-8 bg-slate-100">
-        <div className="userPageText md:my-6 text-gray-600">
+      <div className="font-fira py-4 bg-slate-100">
+        <div className="userPageText px-4 bg-orange-300 md:my-6 text-gray-600">
           <img
-            className=" rounded-full inline-block w-28 md:w-40"
+            className="rounded-full inline-block w-28 md:w-40"
             src="/billie.webp"
             alt=""
           />
           <p className="py-2 text-3xl">Hello {props.userName}</p>
           <p className="text-lg">{props.email}</p>
+          <button onClick={handleSignOut}>Sign Out</button>
         </div>
-        <button onClick={handleSignOut}>Sign Out</button>
         {!isMyFollowersClicked ? (
-          <div>
+          <div className="px-8">
             <div className="aboutMe">
               <p className="text-orange-500">ABOUT ME</p>
               <textarea
@@ -49,15 +188,22 @@ function UserPage(props) {
               <div className="grid grid-cols-2">
                 {' '}
                 <p className="heading">ON THE WEB</p>
-                <button className="w-[25%] bg-orange-500 text-white border-none hover:bg-orange-400 hover:text-gray-100 rounded-lg px-2 py-1 focus:bg-orange-500">
-                  Edit
+                <button
+                  className="w-[25%] bg-orange-500 text-white border-none hover:bg-orange-400 hover:text-gray-100 rounded-lg px-2 py-1 focus:bg-orange-500"
+                  onClick={handleWebEdit}
+                  type={!webInput ? 'submit' : 'button'}
+                >
+                  {webButton}
                 </button>
               </div>
 
               <form className="grid grid-cols-1">
                 <label htmlFor="linkedIn">LinkedIn</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={webInput}
+                  value={linkedin}
+                  onChange={handleLinkedinChange}
                   type="text"
                   name=""
                   placeholder="LinkedIn"
@@ -65,7 +211,10 @@ function UserPage(props) {
                 />
                 <label htmlFor="github">GitHub</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={webInput}
+                  value={github}
+                  onChange={handleGithubChange}
                   type="text"
                   name=""
                   placeholder="GitHub"
@@ -73,7 +222,10 @@ function UserPage(props) {
                 />{' '}
                 <label htmlFor="facebook">Facebook</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={webInput}
+                  value={facebook}
+                  onChange={handleFacebookChange}
                   type="text"
                   name=""
                   placeholder="Facebook"
@@ -81,7 +233,10 @@ function UserPage(props) {
                 />{' '}
                 <label htmlFor="twitter">Twitter</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={webInput}
+                  value={twitter}
+                  onChange={handleTwitterChange}
                   type="text"
                   name=""
                   placeholder="Twitter"
@@ -89,7 +244,10 @@ function UserPage(props) {
                 />{' '}
                 <label htmlFor="instagram">Instagram</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={webInput}
+                  value={instagram}
+                  onChange={handleInstaChange}
                   type="text"
                   name=""
                   placeholder="Instagram"
@@ -97,7 +255,10 @@ function UserPage(props) {
                 />{' '}
                 <label htmlFor="website">Website</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={webInput}
+                  value={website}
+                  onChange={handleWebsiteChange}
                   type="text"
                   name=""
                   placeholder="Your Website"
@@ -108,14 +269,21 @@ function UserPage(props) {
             <div className="professionalInfo">
               <div className="grid grid-cols-2">
                 <p>PROFESSIONAL INFORMATION</p>
-                <button className="w-[25%] bg-orange-500 text-white border-none hover:bg-orange-400 hover:text-gray-100 rounded-lg px-2 py-1 focus:bg-orange-500">
-                  Edit
+                <button
+                  className="w-[25%] bg-orange-500 text-white border-none hover:bg-orange-400 hover:text-gray-100 rounded-lg px-2 py-1 focus:bg-orange-500"
+                  onClick={handleInfoEdit}
+                  type={!infoInput ? 'submit' : 'button'}
+                >
+                  {infoButton}
                 </button>
               </div>
               <form className="grid grid-cols-1">
                 <label htmlFor="education">Highest Education</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={infoInput}
+                  value={education}
+                  onChange={handleEducationChange}
                   type="text"
                   name=""
                   placeholder="Graduation"
@@ -123,7 +291,10 @@ function UserPage(props) {
                 />
                 <label htmlFor="college">What do you do currently?</label>
                 <input
-                  className="rounded m-1"
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={infoInput}
+                  value={college}
+                  onChange={handleCollegeChange}
                   type="text"
                   name=""
                   placeholder="College Student"
@@ -134,13 +305,21 @@ function UserPage(props) {
             <div className="security">
               <div className="grid grid-cols-2">
                 <p>PASSWORD & SECURITY</p>
-                <button className="w-[25%] bg-orange-500 text-white border-none hover:bg-orange-400 hover:text-gray-100 rounded-lg px-2 py-1 focus:bg-orange-500">
-                  Change
+                <button
+                  className="w-[25%] bg-orange-500 text-white border-none hover:bg-orange-400 hover:text-gray-100 rounded-lg px-2 py-1 focus:bg-orange-500"
+                  onClick={handlePasswordEdit}
+                  type={!passwordInput ? 'submit' : 'button'}
+                >
+                  {passwordButton}
                 </button>
               </div>
               <form>
                 <label htmlFor="password">Password</label>
                 <input
+                  className="rounded focus:outline-none px-1 m-1"
+                  disabled={passwordInput}
+                  value={password}
+                  onChange={handlePasswordChange}
                   type="password"
                   name=""
                   placeholder="**********"
