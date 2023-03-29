dab5b64cb365a0bad25429ceb1bb528fa3b6c4b1
Shahan.One <shahanhussain9105@gmail.com>
2023-03-29 23:41:02 +0530

    minor styles change

--- src/components/MyFollowers.jsx
+++ src/components/MyFollowers.jsx
@@ -4,7 +4,7 @@ function MyFollowers() {
   return (
     <div className="grid grid-cols-4 gap-2 text-center px-8">
       {followers.map((follower) => (
-        <div className="bg-slate-400 px-2 py-4 rounded-lg shadow-lg">
+        <div className="bg-slate-400 p-4 m-4 rounded-lg shadow-lg">
           <center>
             <img src="/accounts.png" className="w-20 h-20" alt="" />
           </center>
